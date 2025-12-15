6d6939162e6fabf8cc460ea7e80c86c34e1989aa
jack-frings <jackdfrings@protonmail.com>
2025-12-15 11:49:39 -0800

    first

---
+++ src/Dependencies.lua
@@ -0,0 +1,26 @@
+--[[
+    GD50
+    Angry Birds
+    
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+Class = require 'lib/class'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+
+require 'src/constants'
+require 'src/Level'
+require 'src/StateMachine'
+
+require 'src/states/BaseState'
+require 'src/states/PlayState'
+require 'src/states/StartState'
+
+gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32),
+    ['huge'] = love.graphics.newFont('fonts/font.ttf', 64)
+}
